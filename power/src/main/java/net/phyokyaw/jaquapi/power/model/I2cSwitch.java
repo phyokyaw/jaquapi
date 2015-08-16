7ba01218e959ea6cebb6f22e372428f606789deb
Phyo Kyaw <phyokyaw.uk@gmail.com>
2015-08-17 00:19:07 +0100

    update

--- power/src/main/java/net/phyokyaw/jaquapi/power/model/I2cSwitch.java
+++ power/src/main/java/net/phyokyaw/jaquapi/power/model/I2cSwitch.java
@@ -1,17 +1,60 @@
 package net.phyokyaw.jaquapi.power.model;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.phyokyaw.jaquapi.core.model.Operatable;
 
 public class I2cSwitch implements Operatable {
+	private static Logger logger = LoggerFactory.getLogger(I2cSwitch.class);
+	private final int id;
 
+	public I2cSwitch(int id) {
+		this.id = id;
+	}
 	@Override
 	public void setOn(boolean isOn) {
-		if (isOn) {
-			//
-		} else {
-			//
+		String newVal = null;
+		try {
+			Runtime r = Runtime.getRuntime();
+			Process p;
+			String line;
+			p = r.exec(new String[]{"ssh", "pi@192.168.0.11", "/usr/sbin/i2cget", "-y", "1", "0x20", "0x01"});
+			BufferedReader is = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			if ((line = is.readLine()) != null) {
+				String currentHex = line.substring(2);
+				String bits = new BigInteger(currentHex, 16).toString(2);
+				char bit = bits.charAt(bits.length() - id - 1);
+				if ((bit == '1' && isOn) || (bit == '0' && !isOn)) {
+					newVal = getHex(id, currentHex);
+				}
+			}
+			System.out.flush();
+			p.waitFor(); // wait for process to complete
+		} catch(Exception ex) {
+			logger.error("Unable to execute remote command", ex);
 		}
+		if (newVal != null) {
+			try {
+				Runtime r = Runtime.getRuntime();
+				Process p;
+				p = r.exec(new String[]{"ssh", "pi@192.168.0.11", "/usr/sbin/i2cset", "-y", "1", "0x20", "0x01", newVal});
+				System.out.flush();
+				p.waitFor(); // wait for process to complete
+			} catch(Exception ex) {
+				logger.error("Unable to execute remote command", ex);
+			}
+		}
+	}
+
+	public static String getHex(int id, String currentVal) {
+		int val = (int) Math.pow(2, id);
+		return "0x" + Integer.toHexString(~(~Integer.parseInt(currentVal, 16) ^ val));
 	}
 
 	@Override
@@ -21,6 +64,6 @@ public class I2cSwitch implements Operatable {
 
 	@Override
 	public String toString() {
-		return "";
+		return Integer.toString(id);
 	}
 }
--- temperature/src/main/java/net/phyokyaw/jaquapi/temperature/services/TemperatureService.java
+++ temperature/src/main/java/net/phyokyaw/jaquapi/temperature/services/TemperatureService.java
@@ -2,20 +2,17 @@ package net.phyokyaw.jaquapi.temperature.services;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ScheduledFuture;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -72,32 +69,32 @@ public class TemperatureService implements AquaService {
 
 	private void update() {
 		logger.debug("Updating temp value");
-//		try {
-//			Runtime r = Runtime.getRuntime();
-//			Process p;
-//			String line;
-//			p = r.exec(new String[]{"ssh", "pi@192.168.0.11", "less", "/sys/bus/w1/devices/28-031466113fff/w1_slave"});
-//			BufferedReader is = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//			StringBuilder builder = new StringBuilder();
-//			while ((line = is.readLine()) != null) {
-//				builder.append(line);
-//			}
-//			Pattern pattern = Pattern.compile(".*t=(\\d+)");
-//			Matcher matcher = pattern.matcher(builder.toString());
-//			if (matcher.find()) {
-//				value = Double.parseDouble(matcher.group(1)) / 1000;
-//			} else {
-//				throw new Exception("Unable to get Temperature data");
-//			}
-//			System.out.flush();
-//			p.waitFor(); // wait for process to complete
-//			logger.info("Updating ph value with: " + value);
-//		} catch (Exception e) {
-//			logger.error("Error executing ph reader", e);
-//		}
-		value = Double.valueOf(new DecimalFormat("#.##").format(23 + (new Random().nextDouble() * 2)));
-		
-		
+		try {
+			Runtime r = Runtime.getRuntime();
+			Process p;
+			String line;
+			p = r.exec(new String[]{"ssh", "pi@192.168.0.11", "less", "/sys/bus/w1/devices/28-031466113fff/w1_slave"});
+			BufferedReader is = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuilder builder = new StringBuilder();
+			while ((line = is.readLine()) != null) {
+				builder.append(line);
+			}
+			Pattern pattern = Pattern.compile(".*t=(\\d+)");
+			Matcher matcher = pattern.matcher(builder.toString());
+			if (matcher.find()) {
+				value = Double.parseDouble(matcher.group(1)) / 1000;
+			} else {
+				throw new Exception("Unable to get Temperature data");
+			}
+			System.out.flush();
+			p.waitFor(); // wait for process to complete
+			logger.info("Updating ph value with: " + value);
+		} catch (Exception e) {
+			logger.error("Error executing ph reader", e);
+		}
+		//		value = Double.valueOf(new DecimalFormat("#.##").format(23 + (new Random().nextDouble() * 2)));
+		//
+
 	}
 
 	//	private String getTempFilePath() {
@@ -118,7 +115,7 @@ public class TemperatureService implements AquaService {
 
 	public List<TemperatureRecord> getLastRecords(HistoryInterval days) {
 		Calendar calendar = new GregorianCalendar();
-		
+
 		List<TemperatureRecord> filteredRecords = new ArrayList<TemperatureRecord>();
 		if (days == HistoryInterval.HOUR) {
 			calendar.add(Calendar.HOUR_OF_DAY, -1);
