9e559f81d70739bba79a35fd98ec03d00ca8998d
Phyo Kyaw <phyokyaw.uk@gmail.com>
2015-08-24 07:40:43 +0100

    More.

--- web/src/main/java/META-INF/webapp/WEB-INF/views/device_detail.jsp
+++ web/src/main/java/META-INF/webapp/WEB-INF/views/device_detail.jsp
@@ -4,17 +4,9 @@
 <head>
 <title>Jaqua-pi aquarium controller</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css"
-	href="<c:url value='/c/jquery.mobile-1.4.5.min.css' />" />
-<link rel="stylesheet" type="text/css"
-	href="<c:url value='/c/mobile.css' />" />
-<script type="text/javascript"
-	src="<c:url value='/s/jquery-1.11.0.min.js' />"></script>
-<script type="text/javascript"
-	src="<c:url value='/s/jquery.mobile-1.4.5.min.js' />"></script>
 </head>
 <body>
-	<div data-role="page">
+	<div id="device_detail_page" data-role="page">
 		<div data-role="header" data-add-back-btn="true"
 			style="overflow: hidden;">
 			<h4>${device.name}</h4>
@@ -42,12 +34,17 @@
 				</div>
 				<div class="ui-block-b" style="width:30%">
 					<form>
-						<input type="checkbox" data-role="flipswitch" name="flip-checkbox-feed" id="flip-checkbox-feed" <c:if test="${device.mode.shouldBeOn()}">checked</c:if> />
+						<input type="checkbox" data-role="flipswitch" name="flip-checkbox-default_on_off" id="flip-checkbox-default_on_off" <c:if test="${device.mode.shouldBeOn()}">checked</c:if> />
 					</form>
 				</div>
 			</div>
 		</div>
 		<!-- /content -->
+		<script>
+			$(document).on("pageinit", "#device_detail_page", function(event) {
+				
+			});
+		</script>
 	</div>
 	<!-- /page -->
 
--- web/src/main/java/net/phyokyaw/jaquapi/web/PowerWebControl.java
+++ web/src/main/java/net/phyokyaw/jaquapi/web/PowerWebControl.java
@@ -1,12 +1,5 @@
 package net.phyokyaw.jaquapi.web;
 
-import net.phyokyaw.jaquapi.core.model.Device;
-import net.phyokyaw.jaquapi.core.model.OnOffMode;
-import net.phyokyaw.jaquapi.programme.model.Programme;
-import net.phyokyaw.jaquapi.programme.services.PowerControlDeviceService;
-import net.phyokyaw.jaquapi.programme.services.PowerControlDeviceService.DeviceStatus;
-import net.phyokyaw.jaquapi.programme.services.PowerControlDeviceService.ProgrammeStatus;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.phyokyaw.jaquapi.core.model.Device;
+import net.phyokyaw.jaquapi.core.model.OnOffMode;
+import net.phyokyaw.jaquapi.programme.model.Programme;
+import net.phyokyaw.jaquapi.programme.services.PowerControlDeviceService;
+import net.phyokyaw.jaquapi.programme.services.PowerControlDeviceService.DeviceStatus;
+import net.phyokyaw.jaquapi.programme.services.PowerControlDeviceService.ProgrammeStatus;
+
 @Controller
 public class PowerWebControl {
 
 	private static Logger logger = LoggerFactory.getLogger(PowerWebControl.class);
-	
+
 	@Autowired
 	@Qualifier("programme")
 	private PowerControlDeviceService powerControlDeviceService;
@@ -35,20 +35,20 @@ public class PowerWebControl {
 		}
 		return deviceStatus;
 	}
-	
+
 	@RequestMapping("/device/{id}")
 	public @ResponseBody ModelAndView getDeviceDetail(@PathVariable("id") long id) {
 		ModelAndView mav = new ModelAndView("device_detail");
 		mav.addObject("device", powerControlDeviceService.getDevice(id));
 		return mav;
 	}
-	
-	@RequestMapping("/device/{id}/{on}/{duration}")
+
+	@RequestMapping("/device_override/{id}/{on}/{duration}")
 	public @ResponseBody boolean deviceOverride(@PathVariable("id") long id, @PathVariable("on") boolean on, @PathVariable("id") long duration) {
 		powerControlDeviceService.getDevice(id).setOverridingMode(new OnOffMode(on), duration * 1000);
 		return true;
 	}
-	
+
 	@RequestMapping("/feed_info")
 	public @ResponseBody ModelAndView feedInfo() {
 		ModelAndView mav = new ModelAndView("feed");
@@ -59,24 +59,24 @@ public class PowerWebControl {
 		}
 		return mav;
 	}
-	
+
 	@RequestMapping("/programmes")
 	public @ResponseBody ModelAndView getProgrammes() {
 		ModelAndView mav = new ModelAndView("maintenance");
 		mav.addObject("programmes", powerControlDeviceService.getProgrammes());
 		return mav;
 	}
-	
+
 	@RequestMapping("/activate_programme/{id}")
 	public void activateProgramme(@PathVariable("id") long id) {
 		powerControlDeviceService.activateProgramme(id);
 	}
-	
+
 	@RequestMapping("/deactivate_programme/{id}")
 	public void deactivateProgramme(@PathVariable("id") long id) {
 		powerControlDeviceService.deactivateProgramme(id);
 	}
-	
+
 	@RequestMapping("/programme_status/{id}")
 	public @ResponseBody ProgrammeStatus programmeStatus(@PathVariable("id") long id) {
 		Programme programme = powerControlDeviceService.getProgramme(id);
