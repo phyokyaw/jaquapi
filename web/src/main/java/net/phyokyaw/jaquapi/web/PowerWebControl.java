54c2b5349374be2b9d400907569ec3c2d6fb0e0f
Phyo Kyaw <phyokyaw.uk@gmail.com>
2015-09-02 15:32:56 +0100

    security

--- web/src/main/java/net/phyokyaw/jaquapi/web/PowerWebControl.java
+++ web/src/main/java/net/phyokyaw/jaquapi/web/PowerWebControl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ public class PowerWebControl {
 	@Qualifier("programme")
 	private PowerControlDeviceService powerControlDeviceService;
 
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@RequestMapping("/device_status")
 	public @ResponseBody DeviceStatus[] getDeviceStatus() {
 		Device devices[] = powerControlDeviceService.getDevices();
