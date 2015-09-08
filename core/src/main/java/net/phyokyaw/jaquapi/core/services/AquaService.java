36f18821e8f41789ffa95148d1f3f11ee945ef10
Phyo Kyaw <phyokyaw.uk@gmail.com>
2015-09-08 07:27:08 +0100

    More.

--- core/src/main/java/net/phyokyaw/jaquapi/core/services/AquaService.java
+++ core/src/main/java/net/phyokyaw/jaquapi/core/services/AquaService.java
@@ -1,7 +1,7 @@
 package net.phyokyaw.jaquapi.core.services;
 
 public interface AquaService {
-	public static final String DEVICE_SSH_ADDR = "pi@192.168.0.123";
+	public static final String DEVICE_SSH_ADDR = "pi@192.168.0.100";
 	public enum HistoryInterval{HOUR, DAY, WEEK, MONTH};
 	double getValue();
 }
