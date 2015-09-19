1c58e0c54755ace9941b0221c5bb3ed30f21fba9
Phyo Kyaw <phyokyaw.uk@gmail.com>
2015-09-19 22:43:41 +0100

    bug.

--- power/src/main/java/net/phyokyaw/jaquapi/power/model/I2cSwitch.java
+++ power/src/main/java/net/phyokyaw/jaquapi/power/model/I2cSwitch.java
@@ -61,6 +61,9 @@ public class I2cSwitch implements Operatable, ValueUpdateListener {
 	@Override
 	public void setValue(String value) {
 		currentHex = value.substring(2);
+		if (currentHex.length() == 3) {
+			currentHex = "0" + currentHex;
+		}
 	}
 
 	@Override
