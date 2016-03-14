828b18287d99ff3b6d69cc85f66c96a9295e66f1
Phyo Kyaw <phyokyaw.uk@gmail.com>
2016-03-14 23:26:37 +0000

    more

--- scripts/testgpio.py
+++ scripts/testgpio.py
@@ -2,23 +2,33 @@
 
 import RPi.GPIO as GPIO  
 import paho.mqtt.client as mqtt
+import spidev
 
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+def ReadChannel(channel):
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
 
 client = mqtt.Client()
 
 def my_callback(channel):
     (result, mid) = client.publish("switch %d" % channel, GPIO.input(channel))
+    (result, mid) = client.publish("switch ph", ReadChannel(0))
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.will_set("clients/monitor", "0", 0, retain=True)
-    print("will sent")
-    client.subscribe("switch %d" % 16, 0)
-    print("subscribed")
     client.publish("clients/monitor", "1", 0, retain=True)
-    print("client published")
+    client.subscribe("switch %d" % 16, 0)
+    client.subscribe("switch ph", 0)
+    
 
 def on_message(client, userdata, msg):
 	print(msg.topic+" "+str(msg.payload))
