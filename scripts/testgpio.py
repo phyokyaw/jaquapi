86ea0f162bafea5045c5e8efca6a7cf188acc96e
Phyo Kyaw <phyokyaw.uk@gmail.com>
2016-03-15 19:23:19 +0000

    start

--- scripts/testgpio.py
+++ scripts/testgpio.py
@@ -10,9 +10,9 @@ from os.path import isfile
 GPIO.setmode(GPIO.BCM)
 
 device_name = "fishtank"
-gpio_switches = [22, 23, 33, 34, 16]
+gpio_switches = [5, 6, 12, 13, 16]
 ph_analog_channel = 0
-report_inteval_in_sec = 60
+report_inteval_in_sec = 10
 user_name = 'admin'
 password = 'password'
 mqtt_server_ip = "192.168.0.11"
@@ -20,9 +20,6 @@ mqtt_server_port = 61613
 
 for key in gpio_switches:
     GPIO.setup(key, GPIO.IN)
-
-for key in gpio_switches:
-    GPIO.add_event_detect(key, GPIO.FALLING, callback=switch_changed, bouncetime=300)
 
 # Open SPI bus
 spi = spidev.SpiDev()
@@ -36,10 +33,12 @@ def publish_ph():
     phdata = 3.5 * ((data * 5.0) / float(1023))
     phdata = round(phdata, 2)
     (result, mid) = client.publish(device_name + "/ph", phdata)
+    print "ph " + str(phdata)
 
 def publish_temperature():
     for f in listdir("/sys/bus/w1/devices/"):
     	filename = "/sys/bus/w1/devices/" + f + "/w1_slave"
+        print filename
         if isfile(filename):
             tfile = open(filename) 
             text = tfile.read()
@@ -53,9 +52,9 @@ client = mqtt.Client()
 
 def switch_changed(channel):
     (result, mid) = client.publish(device_name + "/switch/%d" % gpio_switches.index(channel), GPIO.input(channel))
-    
 
 def on_connect(client, userdata, flags, rc):
+    global connected
     if rc == 0:
         connected = True
         client.will_set(device_name + "/connection", "0", 0, retain=True)
@@ -63,6 +62,9 @@ def on_connect(client, userdata, flags, rc):
 
 def on_disconnect(client, userdata, rc):
     connected = False
+
+for key in gpio_switches:
+    GPIO.add_event_detect(key, GPIO.FALLING, callback=switch_changed, bouncetime=300)
 
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
@@ -72,9 +74,10 @@ client.connect(mqtt_server_ip, mqtt_server_port, 60)
 client.loop_start()
 
 while True:
-    time.sleep(report_inteval_in_sec)
-    if connected:
+    global connected
+    if connected == True:
         publish_ph()
         publish_temperature()
+    time.sleep(report_inteval_in_sec)
 
 GPIO.cleanup()           # clean up GPIO on normal exit  
