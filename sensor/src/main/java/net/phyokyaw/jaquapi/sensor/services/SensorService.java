b506e0e31c0f11b09c1d9898ba684d7926b7c540
Phyo Kyaw <phyo.kyaw@bp.renesas.com>
2016-03-17 16:01:51 +0000

    More.

--- sensor/src/main/java/net/phyokyaw/jaquapi/sensor/services/SensorService.java
+++ sensor/src/main/java/net/phyokyaw/jaquapi/sensor/services/SensorService.java
@@ -7,8 +7,6 @@ import javax.annotation.PostConstruct;
 import net.phyokyaw.jaquapi.remote.RemoteMessagingService;
 import net.phyokyaw.jaquapi.sensor.model.SensorDevice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
