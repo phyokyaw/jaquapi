8305bbde910a174cbab7a1c0cca75a650895d5f7
Phyo Kyaw <phyo.kyaw@bp.renesas.com>
2016-04-14 11:24:22 +0100

    More

--- scripts/sensors_mqtt_run.py
+++ scripts/sensors_mqtt_run.py
@@ -6,16 +6,19 @@ import spidev
 import time
 from os import listdir
 from os.path import isfile
+import threading
 
 GPIO.setmode(GPIO.BCM)
 
 device_name = "/fishtank"
 gpio_switches = [5, 6, 12, 13, 16]
 ph_analog_channel = 0
-report_inteval_in_sec = 60
+ph_read_count = 40
+ph_read_delay_in_sec = 0.2
+report_inteval_in_sec = 120
 user_name = 'admin'
 password = 'password'
-mqtt_server_ip = "192.168.0.11"
+mqtt_server_ip = "127.0.0.1"
 mqtt_server_port = 61613
 
 for key in gpio_switches:
@@ -27,17 +30,19 @@ spi.open(0,0)
 
 connected = False
 
-def publish_ph():
+def get_ph():
     adc = spi.xfer2([1,(8+ph_analog_channel)<<4,0])
-    data = ((adc[1]&3) << 8) + adc[2]
-    phdata = 3.5 * ((data * 5.0) / float(1023))
-    phdata = round(phdata, 2)
+    return ((adc[1]&3) << 8) + adc[2]
+
+def publish_ph():
+    phdata = 0 
+    for x in range(0, ph_read_count):
+        phdata += get_ph()
+        time.sleep(ph_read_delay_in_sec)
+    phdata = 3.5 * ((phdata * 5.0) / float(1024))
+    phdata = round(phdata / ph_read_count, 2)
     (result, mid) = client.publish(device_name + "/ph", phdata)
-<<<<<<< HEAD:scripts/testgpio.py
-    print phdata
-=======
     print "ph " + phdata
->>>>>>> e8c0ef53a1fb437215c10a978ccab211cc0b993c:scripts/sensors_mqtt_run.py
 
 def publish_temperature():
     for f in listdir("/sys/bus/w1/devices/"):
@@ -56,12 +61,7 @@ client = mqtt.Client()
 
 def switch_changed(channel):
     (result, mid) = client.publish(device_name + "/switch/%d" % gpio_switches.index(channel), GPIO.input(channel))
-<<<<<<< HEAD:scripts/testgpio.py
-    if GPIO.input(channel) == 1:
-       print "De-De"
-=======
     print "channel " + channel + ": " + GPIO.input(channel)
->>>>>>> e8c0ef53a1fb437215c10a978ccab211cc0b993c:scripts/sensors_mqtt_run.py
 
 def on_connect(client, userdata, flags, rc):
     global connected
@@ -69,14 +69,9 @@ def on_connect(client, userdata, flags, rc):
         connected = True
         client.will_set(device_name + "/connection", "0", 0, retain=True)
         client.publish(device_name + "/connection", "1", 0, retain=True)
-<<<<<<< HEAD:scripts/testgpio.py
-	publish_ph()
-	publish_temperature()
-=======
         for key in gpio_switches:
             switch_changed(key)
         publish_sensors()
->>>>>>> e8c0ef53a1fb437215c10a978ccab211cc0b993c:scripts/sensors_mqtt_run.py
 
 def on_disconnect(client, userdata, rc):
     global connected
@@ -87,38 +82,27 @@ def publish_sensors():
     publish_temperature()
 
 for key in gpio_switches:
-<<<<<<< HEAD:scripts/testgpio.py
-    GPIO.add_event_detect(key, GPIO.FALLING, callback=switch_changed, bouncetime=10)
-=======
     GPIO.add_event_detect(key, GPIO.FALLING, callback=switch_changed, bouncetime=50)
->>>>>>> e8c0ef53a1fb437215c10a978ccab211cc0b993c:scripts/sensors_mqtt_run.py
+
 
 client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 
-client.username_pw_set(user_name,password)
+client.username_pw_set(user_name, password)
 client.connect(mqtt_server_ip, mqtt_server_port, 60)
 client.loop_start()
-<<<<<<< HEAD:scripts/testgpio.py
-print "started"
-while True:
-    if connected == True:
-        publish_ph()
-        publish_temperature()
-    time.sleep(report_inteval_in_sec)
 
-GPIO.cleanup()           # clean up GPIO on normal exit  
-=======
 
 try:
 	while True:
 	    if connected == True:
-	    	publish_sensors
+	        t1 = threading.Thread(target=publish_sensors)
+	        t1.start()
+	        t1.join()
 	    time.sleep(report_inteval_in_sec)
 except KeyboardInterrupt:  
     # here you put any code you want to run before the program   
     # exits when you press CTRL+C
     client.disconnect()
 finally:
-    GPIO.cleanup()           # clean up GPIO on normal exit  
->>>>>>> e8c0ef53a1fb437215c10a978ccab211cc0b993c:scripts/sensors_mqtt_run.py
+    GPIO.cleanup()           # clean up GPIO on normal exit
