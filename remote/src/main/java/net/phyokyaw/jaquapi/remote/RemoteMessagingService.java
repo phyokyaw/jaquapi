cba390ca61f627c0499d75be5749964dcc8910cb
Phyo Kyaw <phyo.kyaw@bp.renesas.com>
2016-04-14 16:53:08 +0100

    more

--- remote/src/main/java/net/phyokyaw/jaquapi/remote/RemoteMessagingService.java
+++ remote/src/main/java/net/phyokyaw/jaquapi/remote/RemoteMessagingService.java
@@ -25,7 +25,7 @@ public class RemoteMessagingService {
 
 	private static final String PASSWORD = "password";
 	private static final String USER_NAME = "admin";
-	private final String broker = "tcp://127.0.0.1:61613";
+	private final String broker = "tcp://localhost:1883";
 	private final String clientId = "tankcontrol";
 	private final MemoryPersistence persistence = new MemoryPersistence();
 	private static final String FISH_TANK_CONNECTION = "/fishtank/connection";
